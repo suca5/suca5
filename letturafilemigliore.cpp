b0f654cadb68c3bf53c19698ad0501098df1de21
suca5 <167331742+suca5@users.noreply.github.com>
2024-04-22 16:27:19 +0200

    Update letturafilemigliore.cpp

--- letturafilemigliore.cpp
+++ letturafilemigliore.cpp
@@ -34,27 +34,33 @@ void read_data_formatRECO2(std::vector<std::string> listfiles, std::vector<std::
             int type = std::stoi(linedata[3]);
             if (type == 20) {
                 // Process RICH ring data
-                int hypo = std::stoi(linedata[4]);
+                //int hypo = std::stoi(linedata[4]);
                 float x = std::stof(linedata[5]);
                 float y = std::stof(linedata[6]);
                 float r = std::stof(linedata[7]);
-                int rhit = std::stoi(linedata[9]);
-                std::vector<int> rlist(linedata.begin() + 10, linedata.begin() + 10 + rhit); //SBAGLIATO INSERIRE FOR PER INSERIRE ELEMNTI SU RHIT
+                //int rhit = std::stoi(linedata[9]);
+                //std::vector<int> rlist(linedata.begin() + 10, linedata.begin() + 10 + rhit); //SBAGLIATO INSERIRE FOR PER INSERIRE ELEMNTI SU RHIT
 
                 // Debug print
                 // std::cout << "RING: hyp " << hypo << " X " << x << " Y " << y << " R " << r << " Nhit " << rhit << std::endl;
                 // for (int hit : rlist) std::cout << hit << " ";
                 // std::cout << std::endl;
-            } else if (type == 23) {
+            } else 
+            if (type == 23) {
                 // Process summary line with from RICH reco
+                std::string id_ev = (linedata[0])
                 int nt = std::stoi(linedata[4]);
-                int el = std::stoi(linedata[5]);
-                int mu = std::stoi(linedata[6]);
-                int pi = std::stoi(linedata[7]);
-                int ka = std::stoi(linedata[8]);
+                //int el = std::stoi(linedata[5]);
+               // int mu = std::stoi(linedata[6]);
+               // int ni = std::stoi(linedata[7]);
+               // int ka = std::stoi(linedata[8]);
                 int ehit = std::stoi(linedata[9]);
                 if (ehit > MAXHIT) ehit = MAXHIT;
-                std::vector<int> elist(linedata.begin() + 10, linedata.begin() + 10 + ehit); //SBAGLIATO INSERIRE FOR PER INSERIRE ELEMNTI SU RHIT
+                std::vector<int> elist(ehit,0)
+                
+                for (int f = 10; f < (10 + ehit), ++f){
+                    elist[f-10]= std::stoi(linedata[f]);                    
+                } 
 
                 // Prepare feature list
                 for (int i = 0; i < ehit; ++i) {
@@ -64,7 +70,7 @@ void read_data_formatRECO2(std::vector<std::string> listfiles, std::vector<std::
                 // Update counters
                 nevt += 1;
                 nrings += nt;
-                nelectrons += mu;
+               // nelectrons += mu;
 
                 // Debug print
                 // std::cout << "EVENT: nTracks: " << nt << "(el " << el << " mu " << mu << " pi " << pi << " ka " << ka << ") Nhit " << ehit << std::endl;
@@ -72,23 +78,23 @@ void read_data_formatRECO2(std::vector<std::string> listfiles, std::vector<std::
                 // Write features and labels on output vectors
                 hitList.push_back(hits_array);
                 labelList1.push_back(nt);
-                labelList2.push_back(el);
+                //labelList2.push_back(el);
 
                 // Check enough statistics was collected
                 if (nevt >= samples) {
                     totevt += nevt;
                     totrings += nrings;
-                    totelectrons += nelectrons;
+                    //totelectrons += nelectrons;
                     std::cout << "file[" << idx << "] evt = " << nevt << " rings = " << nrings << " electrons = " << nelectrons << std::endl;
                     break;
                 }
-            }
+            } continue;
         }
     }
 
     std::cout << "tot events = " << totevt << std::endl;
     std::cout << "tot rings = " << totrings << std::endl;
-    std::cout << "tot electrons = " << totelectrons << std::endl;
+    //std::cout << "tot electrons = " << totelectrons << std::endl;
 }
 
 int main() {
