15335721fc50dec66a701087f37205f78e00d080
suca5 <167331742+suca5@users.noreply.github.com>
2024-04-23 10:41:46 +0200

    Update algoritmo.cpp

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -4,9 +4,8 @@
 
 extern "C" {
 void increment(hls::stream<ap_axiu<32, 0, 0, 0> >& input, hls::stream<ap_axiu<32, 0, 0, 0> >& output) {
-// For free running kernel, user needs to specify ap_ctrl_none for return port.
-// This will create the kernel without AXI lite interface. Kernel will always be
-// in running states.
+
+    
 #pragma HLS interface ap_ctrl_none port = return
 
     ap_axiu<32, 0, 0, 0> v = input.read();
