3a9f41947e6f0a0ddeef7722869c16e7bc4856c4
suca5 <167331742+suca5@users.noreply.github.com>
2024-05-31 09:48:40 +0200

    Create chiquadro.cpp

---
+++ chiquadro.cpp
@@ -0,0 +1,13 @@
+void chiquadro(float *x, float *y, int *sizes){
+  float x0, y0, r;
+  float chi=0;
+  for(int k; k<1000; k++){
+    for(int temp=0; temp<sizes[i]; temp++){
+      
+        chi+=(r-sqrt())*()/()
+      
+    }
+    temp=0;
+    k += 1;
+  }
+}
